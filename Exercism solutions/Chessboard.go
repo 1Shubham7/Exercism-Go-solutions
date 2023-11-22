a63e3993421a83141c36853e372560449e8c7437
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-22 17:28:53 +0530

    Chessboard.go done

--- Exercism solutions/Chessboard.go
+++ Exercism solutions/Chessboard.go
@@ -1,5 +1,9 @@
 package chessboard
 
+import (
+    // "strconv"
+)
+
 // Declare a type named File which stores if a square is occupied by a piece - this will be a slice of bools
 
 type File []bool
@@ -22,25 +26,39 @@ func CountInFile(cb Chessboard, file string) int {
 // CountInRank returns how many squares are occupied in the chessboard,
 // within the given rank.
 func CountInRank(cb Chessboard, rank int) int {
+    if rank < 1 || rank > 8 {
+        return 0
+    }
         count := 0
-	for i:=0;i<rank;i++{
-		for j:="A";j<="H";j++{
-		for _,occupied := range cb[j]{
-			if occupied{
-				count++
-			}
-
-		}}
+    
+		for _,value := range cb{
+            // since indexing starts from 0, taking length - 1
+            if value[rank-1]{
+                count++
+            }
 	}
 	return count
 }
 
 // CountAll should count how many squares are present in the chessboard.
 func CountAll(cb Chessboard) int {
-	panic("Please implement CountAll()")
+    count := 0
+    for _,value := range cb{
+        count = count + len(value)
+    }
+	return count
 }
 
 // CountOccupied returns how many squares are occupied in the chessboard.
 func CountOccupied(cb Chessboard) int {
-	panic("Please implement CountOccupied()")
+        count := 0
+    for _,value := range cb{
+        // value is []bool
+        for _,boolValue := range value{
+            if boolValue{
+                count++
+            }
+        }
+    }
+	return count
 }
