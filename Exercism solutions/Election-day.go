e4b7b892b431adca663e7515fdf9ae6b2f8b5c7b
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-24 14:18:13 +0530

    Election-day.go 1 task remaining

--- Exercism solutions/Election-day.go
+++ Exercism solutions/Election-day.go
@@ -1,8 +1,9 @@
 package electionday
 
 import (
-    // "fmt"
+    "fmt"
 )
+
 
 // NewVoteCounter returns a new vote counter with
 // a given number of initial votes.
@@ -22,22 +23,30 @@ func VoteCount(counter *int) int {
 
 // IncrementVoteCount increments the value in a vote counter.
 func IncrementVoteCount(counter *int, increment int) {
-
     *counter = *counter + increment
     
 }
 
 // NewElectionResult creates a new election result.
 func NewElectionResult(candidateName string, votes int) *ElectionResult {
-	panic("Please implement the NewElectionResult() function")
+    
+    a:=ElectionResult{
+        Name:	candidateName,
+        Votes:	votes,
+    }
+    return &a
+
+    
 }
 
 // DisplayResult creates a message with the result to be displayed.
 func DisplayResult(result *ElectionResult) string {
-	panic("Please implement the DisplayResult() function")
+    // a := 
+    return fmt.Sprintf("%s (%d)", result.Name, result.Votes)
+    
 }
 
 // DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map.
 func DecrementVotesOfCandidate(results map[string]int, candidate string) {
-	panic("Please implement the DecrementVotesOfCandidate() function")
+    
 }
